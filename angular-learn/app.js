b8b7f8ab1d6ff20820002dcb6e4d103e1bb8366e
cfighter <568949920@qq.com>
2016-04-20 11:11:19 +0800

    angular 模板

--- angular-learn/app.js
+++ angular-learn/app.js
@@ -1,25 +1,28 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
 
 app.controller('phoneListCtr', function($scope, $http) {
-    $http.get('phones.json').success(function(data) {
+    $http.get('phones/phones.json').success(function(data) {
         $scope.phones = data;
     });
     $scope.orderProp = 'age';
 });
-app.controller('phoneDetailCtr', function($scope, $routeParams) {
-    $scope.phoneId=$routeParams.phoneId;
+app.controller('phoneDetailCtr', function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+        $scope.phone = data;
+    });
 });
-app.config(['$routerProvider'],function($routerProvider){
-	$routerProvider
-	.when('/phones',{
-		template:'partials/phone-list.html',
-		controller:'phoneListCtr'
-	})
-	.when('/phones/:phoneId',{
-		template:'partials/phone-detail.html',
-		controller:'phoneDetailCtr'
-	})
-	.oherwise({
-		redirectTo:'/phones'
-	});
-})
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/phones', {
+            templateUrl: 'partials/phone-list.html',
+            controller: 'phoneListCtr'
+        })
+        .when('/phones/:phoneId', {
+            templateUrl: 'partials/phone-detail.html',
+            controller: 'phoneDetailCtr'
+        })
+        .otherwise({
+            redirectTo: '/phones'
+        });
+}])
